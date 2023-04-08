9e68b3237fc24cd8a5f036638529ab52896d9938
Aldiyar <aldiyarmaratov0@gmail.com>
2023-04-08 12:11:29 +0600

    task 6

--- com/company/Main.java
+++ com/company/Main.java
@@ -8,12 +8,31 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int i = scanner.nextInt();
         /*int[] array = new int[n];
           for (int i=0; i<n; i++){
             array[i] = scanner.nextInt();*/
 
-        int min = findFibonacci(n);
-        System.out.println(n+"-th element is " + min);
+        int min = findPow(n,i);
+        System.out.println(n+" in power of "+i+" is " + min);
+    }
+
+    public static int findPow(int n,int i){
+        if(i==1) {
+            return n;
+        }
+        if(i==0){
+            return 1;
+        }
+        if(n==1) {
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+
+        return  n*findPow(n,i-1);
+
     }
 
 
